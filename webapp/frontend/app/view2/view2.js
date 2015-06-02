e26d9fafd999d1e8488fe0f2d120bef67aef6072
César Ricárdez <ricardez.cesar@gmail.com>
2015-06-01 23:38:16 -0500

    experiments with layout

--- webapp/frontend/app/view2/view2.js
+++ webapp/frontend/app/view2/view2.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['ngRoute', 'ngMaterial'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', function($routeProvider, $ngMaterial) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
     controller: 'View2Ctrl'
